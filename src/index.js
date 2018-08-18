f08c759bab2b07df690ffaa21e67ce04fa2168cf
Sam Turrell <sam.turrell@netsells.co.uk>
2018-08-18 19:37:56 +0100

    Not sure what i'm doing here

--- src/index.js
+++
@@ -1,4 +0,0 @@
-/* @flow */
-export default function vuexCrudMutations(input: string) {
-  return input ? `👉 ${input} 👈` : 'No args passed!';
-}
--- src/index.test.js
+++
@@ -1,8 +0,0 @@
-import vuexCrudMutations from './index';
-
-describe('index.js', () => {
-  it('should say something', () => {
-    expect(vuexCrudMutations('🐰')).toEqual('👉 🐰 👈');
-    expect(vuexCrudMutations()).toEqual('No args passed!');
-  });
-});
